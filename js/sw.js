c7873f7c9e9c18dc5b530c91dfcaf0e77b0dce9d
Tommy Sze <tommy@hk.elab.aswatson.com>
2020-08-16 21:19:38 +0800

    no message

---
+++ js/sw.js
@@ -0,0 +1,36 @@
+var cacheName = 'tommy-pwa';
+var filesToCache = [
+  '../index.html',
+  '../page2.html',
+  '../page3.html',
+  '../page4.html',
+  '../css/style.css',
+  '../js/superlargefile.js',
+  '../js/largefile.js',
+  '../js/largefile-larger.js',
+  '../js/largefile-huge.js',
+  '../images/97221-unsplash.jpg',
+  '../images/logo_sprite_5108d_large_2x.jpg',
+  '../images/IMG_8093.jpeg',
+  '../images/umbrella revolution.jpg',
+  '../images/Umbrella Union Poster 5.jpg',
+  '../js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
